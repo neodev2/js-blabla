043e42705e8135a6cc1ed70450aa0fe9df429ca1
neodev2 <undefined>
2017-06-26 02:12:12 +0200

    init

---
+++ client.js
@@ -0,0 +1,36 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+    if (xhr.status == 200) {
+        sortBySkillAndCreateDOMStructure(xhr.response);
+    }
+};
+xhr.send();
+
+function sortBySkillAndCreateDOMStructure(data){
+    for(let i in data){
+        document.body.innerHTML += '<h3>'+i+'</h3>';
+        // sort by skill
+        var array = [];
+        for(let i2 in data[i]){
+            array.push([data[i][i2].skill, data[i][i2].icon, i2]);
+        }
+        array = array.sort(function(a, b){
+            return b[0] - a[0];
+        });
+        // create DOM structure
+        for(let i=0; i<array.length; i++){
+            document.body.innerHTML +=
+            '<div class="item">'+
+                '<div class="icon">'+
+                    '<img src="'+array[i][1]+'">'+
+                    '<div class="skill">'+
+                        '<div class="percentage" style="width: '+array[i][0]+'%; background: '+(array[i][0] >= 50? '#00d800' : '#ff5353')+';"></div>'+
+                    '</div>'+
+                '</div>'+
+                '<small>'+array[i][2]+'</small>'+
+            '</div>';
+        }
+    }
+}
---
+++ img/client.js
@@ -0,0 +1,357 @@
+window.onload = function(){
+	
+	var obj = {
+		"databases": {
+			"mLab": {
+				"icon": "https://img.stackshare.io/service/202/WbvEv6ay.png",
+				"skill": "55"
+			},
+			"MySQL": {
+				"icon": "https://img.stackshare.io/service/1025/logo-mysql-170x170.png",
+				"skill": "65"
+			},
+			"PostgreSQL": {
+				"icon": "https://img.stackshare.io/service/1028/ASOhU5xJ.png",
+				"skill": "58"
+			},
+			"MongoDB": {
+				"icon": "https://img.stackshare.io/service/1030/mX8rO3vw.png",
+				"skill": "60"
+			},
+			"Heroku Postgres": {
+				"icon": "https://img.stackshare.io/service/2516/original.png",
+				"skill": "65"
+			},
+			"ClearDB": {
+				"icon": "https://img.stackshare.io/service/317/data_ok.png",
+				"skill": "65"
+			}
+		},
+		"languages/libraries/frameworks": {
+			"JavaScript": {
+				"icon": "https://img.stackshare.io/service/1209/javascript.jpeg",
+				"skill": "96"
+			},
+			"ES6": {
+				"icon": "https://img.stackshare.io/service/4109/16407404782_8b9c57eab3.jpg",
+				"skill": "90"
+			},
+			"Node.js": {
+				"icon": "http://cremate-dev.de/img/logos/nodejs-new-pantone-black.svg",
+				"skill": "80"
+			},
+			"CSS3": {
+				"icon": "http://www.myiconfinder.com/uploads/iconsets/8b61de4c84033266e15317a6eb9fda2d-css3.png",
+				"skill": "98"
+			},
+			"Less": {
+				"icon": "https://img.stackshare.io/service/1170/3538330.png",
+				"skill": "65"
+			},
+			"Bootstrap": {
+				"icon": "https://img.stackshare.io/service/1101/C9QJ7V3X.png",
+				"skill": "86"
+			},
+			"PHP": {
+				"icon": "https://img.stackshare.io/service/991/php.png",
+				"skill": "76"
+			},
+			"jQuery": {
+				"icon": "https://img.stackshare.io/service/1021/jquery_avatar.png",
+				"skill": "90"
+			},
+			"Angular": {
+				"icon": "https://img.stackshare.io/service/1019/square.png",
+				"skill": "20"
+			},
+			"HTML5": {
+				"icon": "https://img.stackshare.io/service/2538/kEpgHiC9.png",
+				"skill": "90"
+			},
+			"Python": {
+				"icon": "https://img.stackshare.io/service/993/pUBY5pVj.png",
+				"skill": "10"
+			},
+			"jQuery UI": {
+				"icon": "https://img.stackshare.io/service/1022/jqueryui_avatar_96.png",
+				"skill": "90"
+			},
+			"Sass": {
+				"icon": "https://img.stackshare.io/service/1171/jCR2zNJV.png",
+				"skill": "90"
+			},
+			"Express.js": {
+				"icon": "https://camo.githubusercontent.com/fc61dcbdb7a6e49d3adecc12194b24ab20dfa25b/68747470733a2f2f692e636c6f756475702e636f6d2f7a6659366c4c376546612d3330303078333030302e706e67",
+				"skill": "81"
+			},
+			"Socket.IO": {
+				"icon": "https://img.stackshare.io/service/1161/vI0ZZlhZ.png",
+				"skill": "90"
+			},
+			"React Native": {
+				"icon": "https://img.stackshare.io/service/2699/OYIaJ1KK.png",
+				"skill": "60"
+			},
+			"jQuery Mobile": {
+				"icon": "https://img.stackshare.io/service/1023/80508321d8ce3ba8aa264380bb7eba33.png",
+				"skill": "90"
+			},
+			"PhoneGap": {
+				"icon": "https://img.stackshare.io/service/777/NgaEDjHt.jpg",
+				"skill": "60"
+			},
+			"Electron": {
+				"icon": "https://img.stackshare.io/service/2946/XJRdVxx3.png",
+				"skill": "75"
+			},
+			"Cordova": {
+				"icon": "https://img.stackshare.io/service/1716/cordova_256.png",
+				"skill": "80"
+			},
+			"Three.js": {
+				"icon": "https://d13yacurqjgara.cloudfront.net/users/122342/screenshots/1071387/threejs.png",
+				"skill": "70"
+			}
+		},
+		"tools": {
+			"Slack": {
+				"icon": "https://img.stackshare.io/service/675/349dc6f270e53cbe09cd05f6c032fc67.png",
+				"skill": "50"
+			},
+			"Atom": {
+				"icon": "https://img.stackshare.io/service/648/QbnVqgX4.png",
+				"skill": "40"
+			},
+			"Google Apps": {
+				"icon": "https://img.stackshare.io/service/247/-23z8Il1.png",
+				"skill": "80"
+			},
+			"Sequel Pro": {
+				"icon": "https://img.stackshare.io/service/2523/3ab6a1576435ff6c20e3f2694965bbf8.png",
+				"skill": "70"
+			},
+			"Sublime Text": {
+				"icon": "https://img.stackshare.io/service/642/SublimeText_Master_012312_icon.png",
+				"skill": "60"
+			},
+			"VirtualBox": {
+				"icon": "https://img.stackshare.io/service/774/vbox_94px.png",
+				"skill": "80"
+			},
+			"Xcode": {
+				"icon": "https://img.stackshare.io/service/1448/xcode.png",
+				"skill": "68"
+			},
+			"Android Studio": {
+				"icon": "https://img.stackshare.io/service/1447/AyreX9yf.jpeg",
+				"skill": "62"
+			},
+			"Notepad++": {
+				"icon": "https://img.stackshare.io/service/646/logo.png",
+				"skill": "60"
+			},
+			"Tower": {
+				"icon": "https://img.stackshare.io/service/1601/jiDIZ8-s.png",
+				"skill": "75"
+			},
+			"Photoshop": {
+				"icon": "https://media.flaticon.com/img/programs/photoshop@2x.png",
+				"skill": "70"
+			},
+			"Sketch": {
+				"icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Sketch_logo_frame.svg/500px-Sketch_logo_frame.svg.png",
+				"skill": "80"
+			},
+			"Dropbox": {
+				"icon": "https://img.stackshare.io/service/146/cbe630c29a62e283cef31afc5c4471da.png",
+				"skill": "70"
+			},
+			"Git": {
+				"icon": "https://img.stackshare.io/service/1046/git.png",
+				"skill": "70"
+			},
+			"CodeMirror": {
+				"icon": "https://img.stackshare.io/service/2490/E_fCaAi6.png",
+				"skill": "75"
+			},
+			"Brackets": {
+				"icon": "https://img.stackshare.io/service/2226/brackets_512.png",
+				"skill": "60"
+			},
+			"Mongotron": {
+				"icon": "http://mongotron.io/src/images/logo.png",
+				"skill": "70"
+			},
+			"Parallels Desktop": {
+				"icon": "https://therojakplace.com/wp-content/uploads/2016/05/Parallels_Logo-1200x266.png",
+				"skill": "70"
+			},
+			"SSH": {
+				"icon": "https://www.geekdime.com/wp-content/uploads/2015/04/ssh.png",
+				"skill": "80"
+			},
+			"Homebrew": {
+				"icon": "http://brew.sh/img/homebrew-256x256.png",
+				"skill": "65"
+			},
+			"npm": {
+				"icon": "https://img.stackshare.io/service/1120/lejvzrnlpb308aftn31u.png",
+				"skill": "80"
+			},
+			"Craft CMS": {
+				"icon": "https://img.stackshare.io/service/1235/ikJdiVZj.jpeg",
+				"skill": "80"
+			},
+			"TLS": {
+				"icon": "http://parastack.com/wp-content/uploads/2015/07/tls1.png",
+				"skill": "75"
+			},
+			"Twig": {
+				"icon": "http://www.softclear.net/softsite/wp-content/uploads/2014/05/twig-php-template-framework-1.png",
+				"skill": "85"
+			},
+			"RegEx": {
+				"icon": "http://i.imgur.com/CZx0dgG.jpg",
+				"skill": "85"
+			},
+			"AJAX": {
+				"icon": "https://devcentral.f5.com/Portals/0/userfiles/123757/v12-1-AJAX_logo_by_gengns_svg.png",
+				"skill": "85"
+			},
+			"JSON": {
+				"icon": "http://daoinsights.com/wp-content/uploads/2015/08/json-logo.png",
+				"skill": "98"
+			},
+			"Raspberry Pi": {
+				"icon": "https://www.raspberrypi.org/wp-content/uploads/2011/10/Raspi-PGB001.png",
+				"skill": "76"
+			},
+			"WebRTC": {
+				"icon": "http://i.imgur.com/UUookuw.png",
+				"skill": "80"
+			},
+			"WebSockets": {
+				"icon": "http://www.appelsiini.net/demo/websocket/img/HTML5_Connectivity_512.png",
+				"skill": "80"
+			}
+		},
+		"servers": {
+			"Apache": {
+				"icon": "https://img.stackshare.io/service/1051/fab.os.logo.apache.200.15011.jpg",
+				"skill": "70"
+			}
+		},
+		"platforms/services": {
+			"Amazon EC2": {
+				"icon": "https://img.stackshare.io/service/18/amazon-ec2.png",
+				"skill": "30"
+			},
+			"Amazon S3": {
+				"icon": "https://img.stackshare.io/service/25/amazon-s3.png",
+				"skill": "30"
+			},
+			"Heroku": {
+				"icon": "https://img.stackshare.io/service/133/3wgIDj3j.png",
+				"skill": "75"
+			},
+			"MailChimp": {
+				"icon": "https://img.stackshare.io/service/47/69XlPqVT.png",
+				"skill": "70"
+			},
+			"GitHub": {
+				"icon": "https://img.stackshare.io/service/27/sBsvBbjY.png",
+				"skill": "70"
+			},
+			"Trello": {
+				"icon": "https://img.stackshare.io/service/109/JiszgZdZ.png",
+				"skill": "60"
+			},
+			"Google Maps API": {
+				"icon": "https://img.stackshare.io/service/1652/cwzjzCMU.png",
+				"skill": "70"
+			},
+			"Stack Overflow": {
+				"icon": "https://img.stackshare.io/service/1927/apple-touch-icon.png",
+				"skill": "80"
+			}
+		},
+		"operating systems": {
+			"Linux Ubuntu": {
+				"icon": "https://img.stackshare.io/service/3511/cof_orange_hex.jpg",
+				"skill": "65"
+			},
+			"Linux Debian": {
+				"icon": "https://img.stackshare.io/service/1656/vd4gAekh.png",
+				"skill": "80"
+			},
+			"Linux CentOS": {
+				"icon": "https://c1.staticflickr.com/9/8081/8293876400_1a5f90d715.jpg",
+				"skill": "60"
+			},
+			"Linux Mint": {
+				"icon": "https://img.stackshare.io/service/4763/kxYj8-Wg.png",
+				"skill": "30"
+			},
+			"Linux Arch": {
+				"icon": "https://upload.wikimedia.org/wikipedia/commons/1/17/Archlinux-vert-dark.svg",
+				"skill": "70"
+			},
+			"Windows": {
+				"icon": "http://vignette3.wikia.nocookie.net/logopedia/images/a/a5/Windows_logo_-_2002%E2%80%932012_(Multicolored).svg.png/revision/latest/scale-to-width-down/546?cb=20160311150047",
+				"skill": "90"
+			},
+			"Mac OS X": {
+				"icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/OS_X-Logo.svg/862px-OS_X-Logo.svg.png",
+				"skill": "95"
+			}
+		}
+	};
+	
+	
+	// sort by skill and create DOM structure
+	
+	for(let i in obj){
+		
+		document.body.innerHTML += '<h3>'+i+'</h3>';
+		
+		
+		
+		// sort by skill
+		
+		var array = [];
+		
+		for(let i2 in obj[i]){
+			
+			array.push([obj[i][i2].skill, obj[i][i2].icon, i2]);
+			
+		}
+		
+		array = array.sort(function(a, b){
+			return b[0] - a[0];
+		});
+		
+		//console.log(array);
+		
+		
+		
+		// create DOM structure
+		
+		for(let i=0; i<array.length; i++){
+			
+			document.body.innerHTML +=
+			'<div class="item">'+
+				'<div class="icon">'+
+					'<img src="'+array[i][1]+'">'+
+					'<div class="skill">'+
+						'<div class="percentage" style="width: '+array[i][0]+'%"></div>'+
+					'</div>'+
+				'</div>'+
+				'<small>'+array[i][2]+'</small>'+
+			'</div>';
+			
+		}
+		
+	}
+	
+	
+}
