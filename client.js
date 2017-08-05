10446f5ae59f150e75dbc035d00d6a91a9c2fb52
neodev2 <neodev2@gmail.com>
2017-08-05 11:39:34 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-function scaleBetween(unscaledNum, minAllowed, maxAllowed, min, max) {
-	return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+function scaleBetween(unscaledNum, min, max, minAllowed, maxAllowed) {
+    return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
 }
 
 var json = {};
